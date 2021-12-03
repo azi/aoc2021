2ce395df8592de463675096ebae04b1f67a15d2c
Azi Chen <azi.chen@abagile.com>
2021-12-03 14:13:23 +0800

    minor refactor for day2
    - Move the work of prepare data out of reduce

--- src/save_christmas/day2/run.clj
+++ src/save_christmas/day2/run.clj
@@ -2,35 +2,34 @@
  (:require
    [save-christmas.util :as util]))
 
+(def ssample ["forward 5" "down 5" "forward 8" "up 3" "down 8" "forward 2"])
+(def sample  (->> (util/load-data "day2.txt")
+                  (map #(re-find #"(\w+)\s(\d+)" %))
+                  (map #(into [] [(keyword (get % 1)) (util/str->int (get % 2))]))))
+
 ;; part I => 1654760
-(let [sample (util/load-data "day2.txt")
-      result (reduce
-               (fn [result line]
-                 (let [[_ command step] (re-find #"(\w+)\s(\d+)" line)]
-                    (update-in result [(keyword command)] + (util/str->int step))))
-               {:forward 0 :up 0 :down 0}
-               sample)]
+(let [ result (reduce
+                (fn [result [command step]]
+                  (update-in result [command] + step))
+                {:forward 0 :up 0 :down 0}
+                sample)]
 
    (* (:forward result) (- (:down result) (:up result))))
 
 
 ;; part II => 1956047400
-(let [sample (util/load-data "day2.txt")
-      result (reduce
-               (fn [result line]
-                 (let [[_ command _step] (re-find #"(\w+)\s(\d+)" line)
-                        step (util/str->int _step)]
-                   (cond
-                      (= "up" command)      (update-in result [:aim] - step)
-                      (= "down" command)    (update-in result [:aim] + step)
-                      (= "forward" command) (-> result
-                                                (update-in [:forward] + step)
-                                                ((fn [{:keys [aim depth] :as item}]
-                                                    (assoc-in item [:depth] (+ depth (* aim step)))))))))
+(let [result (reduce  ;replace reduce with reductions can see the results of each step
+               (fn [{:keys [aim] :as result} [command step]]
+                (case command
+                   :up      (update-in result [:aim] - step)
+                   :down    (update-in result [:aim] + step)
+                   :forward (-> result
+                                (update-in [:forward] + step)
+                                (update-in [:depth] + (* aim step)))))
                {:forward 0 :depth 0 :aim 0}
                sample)]
 
    (* (:forward result) (:depth result)))
 
-(comment)
- ;; ["forward 5" "down 5" "forward 8" "up 3" "down 8" "forward 2"])]
+(comment
+ ((re-find #"(\w+)\s(\d+)" (first ssample))))
